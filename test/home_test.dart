70cff8a080aae96fc5e6deb2e073c490f2c63410
André <andre.marcelino@parnamirim.rn.gov.br>
2023-12-12 13:19:59 -0300

    add: widget tests

---
+++ test/home_test.dart
@@ -0,0 +1,40 @@
+import 'package:estilizacao_componentes/data/bank_inherited.dart';
+import 'package:estilizacao_componentes/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('My widget has text "Spent"', (widgetTester) async {
+    await widgetTester.pumpWidget(
+      MaterialApp(
+        home: BankInherited(
+          child: const Home(),
+        ),
+      ),
+    );
+    final spentFinder = find.text('Spent');
+    expect(spentFinder, findsOneWidget);
+  });
+
+  testWidgets('Finds a LinearProgressIndicator', (widgetTester) async {
+    await widgetTester.pumpWidget(
+      MaterialApp(
+        home: BankInherited(
+          child: const Home(),
+        ),
+      ),
+    );
+    expect(find.byType(LinearProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Finds a AccountStatus', (widgetTester) async {
+    await widgetTester.pumpWidget(
+      MaterialApp(
+        home: BankInherited(
+          child: const Home(),
+        ),
+      ),
+    );
+    expect(find.byKey(const Key('testKey')), findsOneWidget);
+  });
+}
